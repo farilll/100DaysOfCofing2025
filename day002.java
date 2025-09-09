8b6746dc3acef34269ddee22a2b583266fd9bbae
farilll <rill010101@gmail.com>
2025-09-10 06:33:13 +0800

    mencetak

---
+++ day002.java
@@ -0,0 +1,11 @@
+public class day002 {
+    public static void main(String[] args) {
+        // cetak lalu menambahkan baris baru
+        System.out.println("Faril");
+
+        // cetak tanpa menambahkan baris baru
+        System.out.print("D0223015");
+
+        System.out.printf("\nday : %d", 2);
+    }
+}
