66f934d83560c6545e817357698e0365f1e31f46
farilll <rill010101@gmail.com>
2025-09-10 06:44:46 +0800

    menggunakan komentar

---
+++ day003.java
@@ -0,0 +1,15 @@
+public class day003 {
+    public static void main(String[] args) {
+
+        // Menampilkan teks ke konsol
+        System.out.println("Hello, World!");
+
+        // Operasi matematika sederhana
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+
+        // Menampilkan hasil operasi
+        System.out.println("hasil penjumlahan: " + sum);
+    }
+}
