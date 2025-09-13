fd4ce556c33e2b6ecdce504aff68ddb6ddc63eea
farilll <rill010101@gmail.com>
2025-09-13 20:11:16 +0800

    tipe data angka desimal

---
+++ day006.java
@@ -0,0 +1,9 @@
+public class day006 {
+    public static void main(String[] args) {
+        float a = 10.5f;
+        double b = 20.5;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
