eeec50aada2649a96be704db3427079983447560
farilll <rill010101@gmail.com>
2025-09-14 18:21:16 +0800

    tipe data boolean dan char

---
+++ day007.java
@@ -0,0 +1,12 @@
+public class day007 {
+    public static void main(String[] args) {
+        // char merupakan tipe data karakter yang hanya bisa menampung 1 karakter
+        char kelas = 'B';
+        
+        // boolean merupakan tipe data yang hanya bisa menampung 2 nilai yaitu true atau false
+        boolean mahasiswa = true;
+
+        System.out.println(kelas);
+        System.out.println(mahasiswa);  
+    }
+}
