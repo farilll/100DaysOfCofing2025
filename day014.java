2265a91a828fe25e9419bbe33af5ec1e89caa9bc
farilll <rill010101@gmail.com>
2025-09-21 18:47:30 +0800

    merubah string menjadi tipe data primitif

---
+++ day014.java
@@ -0,0 +1,14 @@
+public class day014 {
+    public static void main(String[] args) {
+        String a = "12.33";
+        String b = "443";
+
+        double x = Double.parseDouble(a);
+        int y = Integer.parseInt(b);
+
+        System.out.println(x);
+        System.out.println(y);
+
+        
+    }
+}
