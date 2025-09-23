eb07f8332ffdce86582715f0c37da7d3fd93bec0
farilll <rill010101@gmail.com>
2025-09-23 23:11:17 +0800

    sisa bagi

---
+++ day017.java
@@ -0,0 +1,10 @@
+public class day017 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int c = a % b;
+
+        System.out.println("sisa bagi : " + c);
+    }
+}
