9fac56d4d6af529cf7a51343d4b93a095978b8b2
farilll <rill010101@gmail.com>
2025-09-23 19:42:29 +0800

    aritmatika kali dan bagi

---
+++ day016.java
@@ -0,0 +1,15 @@
+public class day016 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 2;
+
+        int kali = a * b;
+        int bagi = a / b;
+
+
+        System.out.println("Hasil kali: " + kali);
+        System.out.println("Hasil bagi: " + bagi);
+    }
+}
