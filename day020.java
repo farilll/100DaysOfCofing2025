edfc80207c3d20cbbdaca4e4d1a84a3de5cbfad2
farilll <rill010101@gmail.com>
2025-09-27 19:40:08 +0800

    primitif to string

---
+++ day020.java
@@ -0,0 +1,12 @@
+public class day020 {
+    public static void main(String[] args) {
+        int x = 44;
+        double y = 4;
+
+        String a = String.valueOf(x);
+        String b = String.valueOf(y);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
