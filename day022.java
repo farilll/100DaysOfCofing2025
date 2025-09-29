b4b806bac5efcd2f069d1d9b3bfe8b03453aab1f
farilll <rill010101@gmail.com>
2025-09-29 20:01:30 +0800

    menghitung luas persegi

---
+++ day022.java
@@ -0,0 +1,10 @@
+public class day022 {
+    public static void main(String[] args) {
+        int sisi = 5;
+
+        // rumus luas persegi
+        int luas = sisi * sisi;
+
+        System.out.println("luas persegi dengan sisi " + sisi + " adalah " + luas);
+    }
+}
