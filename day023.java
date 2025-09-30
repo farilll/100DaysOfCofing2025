2944b3ffbd92a214918884d4575d2f615e56efd0
farilll <rill010101@gmail.com>
2025-09-30 21:26:37 +0800

    menghitung luas persegi panjang

---
+++ day023.java
@@ -0,0 +1,11 @@
+public class day023{
+    public static void main(String[] args) {
+
+        int panjang = 5;
+        int lebar = 4;
+        int luas = panjang * lebar;
+
+        System.out.println("Luas Persegi Panjang: " + luas);
+    }
+
+}
