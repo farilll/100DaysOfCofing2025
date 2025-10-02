b774661e7dfe34f5ca20351eb883f07427ae104c
farilll <rill010101@gmail.com>
2025-10-02 21:15:25 +0800

    increment dan decrement

---
+++ day025.java
@@ -0,0 +1,11 @@
+public class day025 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        System.out.println("sebelum di increment kan: " + a++);
+        System.out.println("setelah di increment kan: " + a);
+        System.out.println("==============");
+        System.out.println("sebelum di decrement kan: " + b--);
+        System.out.println("setelah di decrement kan: " + b);
+    }
+}
