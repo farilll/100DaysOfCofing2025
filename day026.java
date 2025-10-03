d8c1f2b4d93f07e45afe81b2389d53580d9afca3
farilll <rill010101@gmail.com>
2025-10-03 22:19:41 +0800

    operator penugasan

---
+++ day026.java
@@ -0,0 +1,20 @@
+public class day026 {
+    public static void main(String[] args) {
+        int a = 3;
+
+        a += 2;
+        System.out.println(a);
+
+        a -= 2;
+        System.out.println(a);
+
+        a *= 2;
+        System.out.println(a);
+
+        a /= 2;
+        System.out.println(a);
+
+        a %= 2;
+        System.out.println(a);
+    }
+}
