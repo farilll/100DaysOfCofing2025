b3eba893db358a8cb85d5af8e239981b68a146f4
farilll <rill010101@gmail.com>
2025-10-05 21:17:26 +0800

    perbandigan

---
+++ day027.java
@@ -0,0 +1,13 @@
+public class day027 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        boolean sama = a == b;
+
+        boolean kebalikan = a != b;
+
+        System.out.printf("apakah a dan b itu nilainya sama ? %s\n", sama);
+        System.out.printf("kebalikan dari nilai di atas :%s", kebalikan);
+    }
+}
