cb8f929d5b3987cf37a620469c62b3adc341415e
farilll <rill010101@gmail.com>
2025-10-06 19:59:13 +0800

    operator perbandingan, besar dan kecil

---
+++ day029.java
@@ -0,0 +1,12 @@
+public class day029 {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 55;
+
+        boolean besar = a > b;
+        boolean kecil = a < b;
+
+        System.out.printf("apakah a lebih besar dari b: %s\n", besar);
+        System.out.printf("apakah a lebih kecil dari b: %s", kecil);
+    }
+}
