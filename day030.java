de0afcb227eee72ba6cb7b53ff6d61f73c00780b
farilll <rill010101@gmail.com>
2025-10-07 22:38:11 +0800

    operator perbandingan

---
+++ day030.java
@@ -0,0 +1,12 @@
+public class day030 {
+    public static void main(String[] args) {
+        int a = 5; 
+        int b = 7;
+
+        boolean samaBesar = a >= b;
+        boolean samaKecil = a <= b;
+
+        System.out.printf("apakah a sama besar dengan b ? : %s\n",samaBesar);
+        System.out.printf("apakah a sama kecil dengan b ? : %s",samaKecil);
+    }
+}
