d142b7a0b223c9fa93080d54de62afc636bd69fd
farilll <rill010101@gmail.com>
2025-10-09 09:52:45 +0800

    logika or

---
+++ day032.java
@@ -0,0 +1,10 @@
+public class day032 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        boolean hasil = a || b;
+
+        System.out.printf("hasil: %s", hasil);
+    }
+}
