1588af74814bf51d13b38b69210bdb8f25eb655f
farilll <rill010101@gmail.com>
2025-10-10 22:53:00 +0800

    operator not

---
+++ day033.java
@@ -0,0 +1,8 @@
+public class day033 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean kebalikan = !a;
+
+        System.out.printf("kebalikan dari nilai a : %s", kebalikan);
+    }
+}
