fcb6e895d779df3cb436e3f5d813693dac2cf6a0
farilll <rill010101@gmail.com>
2025-10-12 20:57:49 +0800

    if

---
+++ day035.java
@@ -0,0 +1,13 @@
+public class day035 {
+    public static void main(String[] args) {
+        int nilai = 0;
+
+        if (nilai > 1) {
+            System.out.println("positif");
+        } else if (nilai == 0) {
+            System.out.println("nol");
+        } else {
+            System.out.println("negatif");
+        }
+    }
+}
