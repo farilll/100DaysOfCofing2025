40d012e217f120c8b3459d8e95d8443dee7cd82a
farilll <rill010101@gmail.com>
2025-10-26 20:27:05 +0800

    ternary

---
+++ day049.java
@@ -0,0 +1,9 @@
+public class day049 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        String hasil = (a > 0) ? "positif" : (a == 0) ? "nol" : "negatif";
+
+        System.out.println(hasil);
+    }
+}
