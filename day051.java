f08c127c0b0abccac30700124215066ec09ae2a1
farilll <rill010101@gmail.com>
2025-10-28 19:05:27 +0800

    looping for

---
+++ day051.java
@@ -0,0 +1,10 @@
+public class day051 {
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i);
+        }
+    }
+}
