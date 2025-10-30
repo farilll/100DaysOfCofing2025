f314c01fbf2fdc1fcc9b96c1b0a5e8c9c9c94102
farilll <rill010101@gmail.com>
2025-10-30 21:47:20 +0800

    break

---
+++ day053.java
@@ -0,0 +1,12 @@
+public class day053 {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 10) {
+            System.out.println(n);
+            if (n == 7) {
+                break;
+            }
+            n++;
+        }
+    }
+}
