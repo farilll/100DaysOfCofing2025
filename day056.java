5a5328c63bf74ee6acbfc2b7b7acf1c14d7c808f
farilll <rill010101@gmail.com>
2025-11-02 23:17:04 +0800

    do while

---
+++ day056.java
@@ -0,0 +1,10 @@
+public class day056 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        do {
+            System.out.println("perulangan ke-" + a);
+            a++;
+        } while (a <= 5);
+    }
+}
