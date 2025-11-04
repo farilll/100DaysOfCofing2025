202a55b13ea4e2c4606189730b50dcabb75085bd
farilll <rill010101@gmail.com>
2025-11-04 20:59:36 +0800

    n-1 dan 1-n

---
+++ day058.java
@@ -0,0 +1,14 @@
+public class day058 {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        for (int i = 1; i <= a; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = a; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
