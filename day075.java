99067cec0437f1044f430ddb04de2e0bae342486
farilll <rill010101@gmail.com>
2025-11-21 21:05:56 +0800

    yayaya

---
+++ day075.java
@@ -0,0 +1,17 @@
+public class day075 {
+    public static void main(String[] args) {
+        String a = "Yooo";
+
+        for (int i = 0; i < a.length(); i++) {
+
+        }
+        System.out.print(a);
+        System.out.println();
+
+        String b = a.toUpperCase();
+        String c = b.toLowerCase();
+
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
