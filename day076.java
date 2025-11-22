58e59922dffedf38ea36f577f9bd3bfbb27aca41
farilll <rill010101@gmail.com>
2025-11-22 20:54:52 +0800

    method

---
+++ day076.java
@@ -0,0 +1,14 @@
+public class day076 {
+    public static void main(String[] args) {
+        String a = "yoo";
+        String b = "aa";
+        String c = "Eff";
+        String d = "EfF";
+
+        System.out.println(a.contains("o"));
+        System.out.println(b.isEmpty());
+        System.out.println(c.equals(d));
+        System.out.println(d.equalsIgnoreCase(c));
+
+    }
+}
