c1fcb645257c1eb732a5c7ee1a24a483aebd59aa
farilll <rill010101@gmail.com>
2025-11-24 23:27:40 +0800

    method

--- day078.java
+++ day078.java
@@ -8,13 +8,9 @@ public class day078 {
         System.out.println("charAt(1) s2 : " + s2.charAt(1)); // 'o' dari "Yooo"
 
         System.out.println("substring(1) of s2: " + s2.substring(1));
-        System.out.println("substring(1,3) of s2: " + s2.substring(1, 3));
 
-        String numbers = "a1b2c3";
-        System.out.println("replaceAll non-digit -> '' : " + numbers.replaceAll("\\D", ""));
-
-        System.out.println("indexOf('o') in s2 : " + s2.indexOf('o'));
-        System.out.println("lastIndexOf('o') in s2 : " + s2.lastIndexOf('o'));
+        System.out.println("indexOf('o') di s2 : " + s2.indexOf('o'));
+        System.out.println("lastIndexOf('o') di s2 : " + s2.lastIndexOf('o'));
 
         System.out.println("concat s2 + \"!\" : " + s2.concat("!"));
 
