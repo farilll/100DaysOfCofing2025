261574c54e7734bb0bc755765e80d4989f859f21
farilll <rill010101@gmail.com>
2025-11-25 19:57:13 +0800

    login

---
+++ day079.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day079 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        String username;
+        String password;
+
+        do {
+            System.out.println("===HALAMAN LOGIN===");
+            System.out.print("Username: ");
+            username = x.nextLine();
+            System.out.print("Password: ");
+            password = x.nextLine();
+        } while (!username.equals("faril") && !password.equals("1234"));
+        System.out.println("welcome!");
+        x.close();
+    }
+}
