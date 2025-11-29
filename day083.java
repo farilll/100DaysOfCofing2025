a8fdb23c34f8d3590cf6d48064de2e484c3ac80d
farilll <rill010101@gmail.com>
2025-11-29 18:50:51 +0800

    menjumlahkan elemen array

---
+++ day083.java
@@ -0,0 +1,12 @@
+public class day083 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4 };
+        int hasil = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            hasil += arr[i];
+
+        }
+        System.out.println(hasil);
+    }
+}
