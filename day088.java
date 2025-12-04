568c9920a7eb133cb6e88028c0f2da6324687321
farilll <rill010101@gmail.com>
2025-12-04 19:47:46 +0800

    array menggunakan for each

---
+++ day088.java
@@ -0,0 +1,9 @@
+public class day088 {
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 5, 6, 67 };
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
