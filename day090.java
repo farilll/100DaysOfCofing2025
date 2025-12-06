f062cc20c76370fcea8eb886366fa2d1f9777f9c
farilll <rill010101@gmail.com>
2025-12-06 22:03:19 +0800

    method non void

---
+++ day090.java
@@ -0,0 +1,11 @@
+public class day090 {
+    static String string(String nama) {
+        return "hai" + nama;
+    }
+
+    public static void main(String[] args) {
+        String hasil = string(" usop");
+
+        System.out.println(hasil);
+    }
+}
