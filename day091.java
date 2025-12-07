5f95c92d8d8d2573b91b74218f8587ee46f61f70
farilll <rill010101@gmail.com>
2025-12-07 22:34:51 +0800

    paramaeter method

---
+++ day091.java
@@ -0,0 +1,9 @@
+public class day091 {
+    static int tambah(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int hasil = tambah(10, 20);
+        System.out.println(hasil);
+    }
+}
