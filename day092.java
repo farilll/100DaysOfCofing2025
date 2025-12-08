02f2a71acb57eabc63f66909b8b55f1b6688d21f
farilll <rill010101@gmail.com>
2025-12-08 21:36:39 +0800

    return

---
+++ day092.java
@@ -0,0 +1,10 @@
+public class day092 {
+    static int kurang(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        int hasil = kurang(30, 15);
+        System.out.println(hasil);
+    }
+}
