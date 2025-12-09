4da5cc724a30fa5e41a3cd29b588a27ce99d3219
farilll <rill010101@gmail.com>
2025-12-09 23:16:43 +0800

    method

---
+++ day093.java
@@ -0,0 +1,16 @@
+public class day093 {
+
+    public static void main(String[] args) {
+        double a = 4.2;
+        double hasil = Math.ceil(a);
+        System.out.println(hasil);
+        double b = 4.9;
+        double hasil2 = Math.floor(b);
+        System.out.println(hasil2);
+        double c = 4.4;
+        double d = 4.6;
+        System.out.println(Math.round(c));
+        System.out.println(Math.round(d));
+
+    }
+}
