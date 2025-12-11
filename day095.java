c6142749fb0ae04a54934cd57ea4a3be769d2a5d
farilll <rill010101@gmail.com>
2025-12-11 23:29:06 +0800

    method void

---
+++ day095.java
@@ -0,0 +1,8 @@
+public class day095 {
+    static void iniMethod(String nama){
+        System.out.println(nama);
+    }
+    public static void main(String[] args) {
+        iniMethod("faril");
+    }
+}
